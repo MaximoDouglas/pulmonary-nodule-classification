cdb8175befc06a41a9469e8159c3625c85e93309
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-02-22 23:31:28 -0300

    Medians, means and stds

--- code/3mm-to-30mm-nodules-data-exploration.py
+++ code/3mm-to-30mm-nodules-data-exploration.py
@@ -13,19 +13,40 @@ df = pandas.read_csv("../data/nodulesFeatures-3mm-30mm.csv")
 
 # Shows shape (rows, columns)
 def getShape():
-    print("Shape: ", end="")
+    print("Shape (rows, columns): ", end="")
     print(df.shape)
 
 # Check for nan fields
 def getNaN():
     count = 0
     for v in df.isna().sum():
-        if v != 0: count+=1
-    print("NaN fields: ", end="")
+        if (v != 0): count+=1
+    print("Missing values: ", end="")
     print(count)
+
+def getNumericFields():
+    numericFields = []
+    for column in df.columns:
+        if (numpy.issubdtype(df[column].dtype, numpy.number)):
+            numericFields.append(column)
+
+    return numericFields
+
+def getStats():
+    numericFields = getNumericFields()
+    print("There are "+ str(len(numericFields)) +
+    " numéric fields. The stats for each one are presented bellow: ")
+
+    for column in numericFields:
+        print(" ." + column + ": ")
+        print('     .median: ' + str(df[column].median()))
+        print('     .mean: ' + str(df[column].mean()))
+        print('     .Standard deviation: ' + str(df[column].std()))
+        print()
 
 def main():
     getShape()
     getNaN()
+    getStats()
 
 main()
