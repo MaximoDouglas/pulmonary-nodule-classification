8b5a441e481796c9d0dd2226177bdd71f31e01bc
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-02-24 16:25:42 -0300

    Minimal ANN - 69.71% acc

--- code/3-30mm-nodules-features-ANN-GS.py
+++ code/3-30mm-nodules-features-ANN-GS.py
@@ -5,9 +5,11 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.optimizers import SGD
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV
 
 # fix random seed for reproducibility
 seed = 1
@@ -29,25 +31,33 @@ encoder.fit(y)
 y = encoder.transform(y) # 0 - BENIGN | 1 - MALIGNANT
 # ------- End data preprocessing
 
+
 # ------- Model archtecture
-epochs = 50
+epochs = 100
 
 # define baseline model
-def baseline_model():
+def baseline_model(units_x=1, units_y=1):
   # create model
   model = Sequential()
-  model.add(Dense(1, input_dim=X.shape[1], activation="sigmoid", kernel_initializer="normal"))
-  model.add(Dense(1, activation="sigmoid", kernel_initializer="normal"))
-  model.add(Dense(1, activation="sigmoid", kernel_initializer="normal"))
+  model.add(Dense(1, input_dim=X.shape[1], activation="relu", kernel_initializer="glorot_uniform"))
+  model.add(Dense(1, activation="relu", kernel_initializer="glorot_uniform"))
+  model.add(Dense(1, activation="sigmoid", kernel_initializer="glorot_uniform"))
 
-  model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+  # Compile model
+  learning_rate = 0.1
+  decay_rate = learning_rate / epochs
+  momentum = 0.8
+
+  # Compile model
+  sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate)
+  model.compile(loss="binary_crossentropy", optimizer=sgd, metrics=['accuracy'])
   return model
 
-estimator = KerasClassifier(build_fn=baseline_model, epochs=epochs, batch_size=X.shape[1])
+estimator = KerasClassifier(build_fn=baseline_model, epochs=epochs, batch_size=250)
 # ------- End model archtecture
 
 # ------- Model validation
-kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+kfold = KFold(n_splits=X.shape[0], shuffle=True, random_state=seed)
 results = cross_val_score(estimator, X, y, cv=kfold, verbose=10)
 
 # Summarize
