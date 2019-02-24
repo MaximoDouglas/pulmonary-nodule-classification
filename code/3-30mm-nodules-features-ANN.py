8e194e93b6cdf36b8de3747685e21ed1b8b006b8
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-02-24 14:47:45 -0300

    Simple first model for de 3 to 30mm nodules

---
+++ code/3-30mm-nodules-features-ANN.py
@@ -0,0 +1,66 @@
+# Multiclass Classification with the Abalone Dataset
+import numpy
+import pandas
+from sklearn.preprocessing import LabelEncoder
+from sklearn import preprocessing
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+
+# fix random seed for reproducibility
+seed = 1
+numpy.random.seed(seed)
+
+# ------- Begin data preprocessing
+
+# load dataset
+df = pandas.read_csv("../data/nodulesFeatures-3mm-30mm.csv")
+dataset = df.drop('id', axis=1).values
+
+# separate features from class
+scaled = dataset[:,:-1].astype(float)
+X = scaled[:,:]
+y = df.values[:,-1]
+
+# transform the class values
+encoder = LabelEncoder()
+encoder.fit(y)
+y = encoder.transform(y) # 0 - BENIGN | 1 - MALIGNANT
+
+# ------- End data preprocessing
+
+
+# ------- Model archtecture
+epochs = 100
+
+# define baseline model
+def baseline_model():
+  # create model
+  model = Sequential()
+  model.add(Dense(73, input_dim=X.shape[1], activation="relu", kernel_initializer="glorot_uniform"))
+  model.add(Dense(32, activation="relu", kernel_initializer="glorot_uniform"))
+  model.add(Dense(3, kernel_initializer="glorot_uniform", activation="sigmoid"))
+
+  # Compile model
+  learning_rate = 0.1
+  decay_rate = learning_rate / epochs
+  momentum = 0.8
+
+  # Compile model
+  sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate)
+  model.compile(loss="sparse_categorical_crossentropy", optimizer=sgd, metrics=['accuracy'])
+  return model
+
+estimator = KerasClassifier(build_fn=baseline_model, epochs=epochs, batch_size=250)
+
+# ------- End model archtecture
+
+kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+results = cross_val_score(estimator, X, y, cv=kfold, verbose=10)
+
+# Summarize
+print("Accuracy: %.2f%%"%(results.mean()*100))
+print("Standard deviation: %.2f%%"%(results.std()*100))
