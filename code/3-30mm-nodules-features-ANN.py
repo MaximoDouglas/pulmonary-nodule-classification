0bd823d75f6772636a54c7877b576b2dc477993c
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-02-24 15:40:44 -0300

    Simple ANN with ADAM optimzer. 1-1-2 got 69.71% add

--- code/3-30mm-nodules-features-ANN.py
+++ code/3-30mm-nodules-features-ANN.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import SGD
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -15,7 +14,6 @@ seed = 1
 numpy.random.seed(seed)
 
 # ------- Begin data preprocessing
-
 # load dataset
 df = pandas.read_csv("../data/nodulesFeatures-3mm-30mm.csv")
 dataset = df.drop('id', axis=1).values
@@ -29,9 +27,7 @@ y = df.values[:,-1]
 encoder = LabelEncoder()
 encoder.fit(y)
 y = encoder.transform(y) # 0 - BENIGN | 1 - MALIGNANT
-
 # ------- End data preprocessing
-
 
 # ------- Model archtecture
 epochs = 100
@@ -42,25 +38,19 @@ def baseline_model():
   model = Sequential()
   model.add(Dense(1, input_dim=X.shape[1], activation="relu", kernel_initializer="glorot_uniform"))
   model.add(Dense(1, activation="relu", kernel_initializer="glorot_uniform"))
-  model.add(Dense(2, kernel_initializer="glorot_uniform", activation="sigmoid"))
+  model.add(Dense(2, activation="sigmoid", kernel_initializer="glorot_uniform"))
 
-  # Compile model
-  learning_rate = 0.1
-  decay_rate = learning_rate / epochs
-  momentum = 0.8
-
-  # Compile model
-  sgd = SGD(lr=learning_rate, momentum=momentum, decay=decay_rate)
-  model.compile(loss="sparse_categorical_crossentropy", optimizer=sgd, metrics=['accuracy'])
+  model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['accuracy'])
   return model
 
 estimator = KerasClassifier(build_fn=baseline_model, epochs=epochs, batch_size=250)
-
 # ------- End model archtecture
 
-kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
+# ------- Model validation
+kfold = KFold(n_splits=X.shape[0], shuffle=True, random_state=seed)
 results = cross_val_score(estimator, X, y, cv=kfold, verbose=10)
 
 # Summarize
 print("Accuracy: %.2f%%"%(results.mean()*100))
 print("Standard deviation: %.2f%%"%(results.std()*100))
+# ------- End model validation
