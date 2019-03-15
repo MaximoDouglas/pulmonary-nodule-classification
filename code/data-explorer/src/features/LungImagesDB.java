e166b4d62936fabcbd0e9b0110ade2b30fea82dd
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-03-14 23:39:50 -0300

    Code refactoring

--- code/data-explorer/src/features/LungImagesDB.java
+++ code/data-explorer/src/features/LungImagesDB.java
@@ -14,8 +14,7 @@ import com.mongodb.MongoClient;
 
 import features.FeaturesNames;
 
-public class LungImagesDB
-{
+public class LungImagesDB{
 
 	private DB db;
 	private DBCollection col;
@@ -24,38 +23,28 @@ public class LungImagesDB
 	public static final String PARENCHYMA_IMAGE = "parenchymaImage";
 	public static final String ORIGINAL_IMAGE = "originalImage";
 
-	private void connectDB(String databaseName)
-	{
-		try
-		{
+	private void connectDB(String databaseName){
+		try{
 			MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
 			db = mongoClient.getDB(databaseName);
 			col = db.getCollection("exams");
 
-		} catch (UnknownHostException e)
-		{
+		} catch (UnknownHostException e){
 			System.err.println("Erro ao tentar se conectar ao banco " + databaseName);
 			e.printStackTrace();
 		} 
 	}
 
-	public LungImagesDB(String databaseName)
-	{
+	public LungImagesDB(String databaseName){
 		this.connectDB(databaseName);
 	}
 
-	/**
-	 * Método que seleciona os documentos de uma dada coleção.
-	 * @param collection
-	 */
-	public void loadCollection(String collection)
-	{
+	public void loadCollection(String collection){
 		if(db.collectionExists(collection))	col = db.getCollection(collection);
 	}
 
 	public void makeCSVfile_solidNodules(String pathName){
-		try
-		{
+		try {
 			pathName += ".csv";
 			File f = new File(pathName);
 			FileWriter fw;
@@ -99,16 +88,12 @@ public class LungImagesDB
 			BasicDBList noduleList;
 			BasicDBList attributeList;
 
-			while(cursor.hasNext())
-			{
+			while(cursor.hasNext()){
 				exam = (BasicDBObject) cursor.next();
 				reading = (BasicDBObject) exam.get("readingSession");
-				noduleList = (BasicDBList) reading.get("bignodule");
-				
-				
+				noduleList = (BasicDBList) reading.get("bignodule");				
 
-				for (int i_nodule = 0; i_nodule < noduleList.size(); i_nodule++)
-				{
+				for (int i_nodule = 0; i_nodule < noduleList.size(); i_nodule++) {
 					nodule = (BasicDBObject) noduleList.get(i_nodule);
 					String texture = (String) nodule.get("texture");
 
@@ -116,6 +101,7 @@ public class LungImagesDB
 					
 					if(nodule.containsField("marginAttributes3D") && nodule.containsField("textureAttributes") 
 							&& (d >= 3) && (d <= 30) && texture.equals("5") && !nodule.get("malignancy").equals("3")){
+						
 						header = new String();
 						
 						String id = exam.getObjectId("_id").toString() + "#";
@@ -123,8 +109,7 @@ public class LungImagesDB
 						header = id + ",";
 
 						attributeList = (BasicDBList) nodule.get("noduleIntensityAttributes3D");
-						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++) 
-						{
+						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++){
 							header += Double.toString((double) attributeList.get(i_attribute)) + ",";
 						}
 
@@ -135,14 +120,12 @@ public class LungImagesDB
 //						}
 
 						attributeList = (BasicDBList) nodule.get("noduleShapeAttributes");
-						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++) 
-						{
+						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++){
 							header += Double.toString((double) attributeList.get(i_attribute)) + ",";
 						}
 
 						attributeList = (BasicDBList) nodule.get("textureAttributes");
-						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++)
-						{
+						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++){
 							header += (String) attributeList.get(i_attribute) + ",";
 						}
 
@@ -153,8 +136,7 @@ public class LungImagesDB
 //						}
 
 						attributeList = (BasicDBList) nodule.get("marginAttributes3D");
-						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++)
-						{
+						for (int i_attribute = 0; i_attribute < attributeList.size(); i_attribute++){
 							if(i_attribute != 0) header += Double.toString((double) attributeList.get(i_attribute)) + ",";
 							else 								 header += Integer.toString((int) attributeList.get(i_attribute)) + ",";
 						}
