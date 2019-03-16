a0f48c0d5a9d5a9ed8b94b1b2332f8be3fb8cb9d
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-03-15 22:24:24 -0300

    Reducing the strings

--- code/data-explorer/src/main/Main.java
+++ code/data-explorer/src/main/Main.java
@@ -17,15 +17,19 @@ public class Main{
 		MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
 		DB db = mongoClient.getDB("db_tcc");
 		
+		String rootPath = "/home/douglas/dev/tcc/data/";
+		String featuresPath = rootPath + "features" + "/";
+		String imagesPath = rootPath + "images" + "/";
+		
 		Features features = new Features(db);
-		features.makeCSVfile_solidNodules("/home/douglas/dev/tcc/data/features/");
-		features.makeCSVfile_solidNodules_withParenchyma("/home/douglas/dev/tcc/data/features/");
+		features.makeCSVfile_solidNodules(featuresPath);
+		features.makeCSVfile_solidNodules_withParenchyma(featuresPath);
 		
 		Segmented_Solid_Nodules ssn_images = new Segmented_Solid_Nodules(db);
-		ssn_images.downloadImages("/home/douglas/dev/tcc/data/images/solid-nodules/");
+		ssn_images.downloadImages(imagesPath + "solid-nodules/");
 		
 		Segmented_Solid_Nodules_With_Attributes ssnwa_images = new Segmented_Solid_Nodules_With_Attributes(db);
-		ssnwa_images.downloadImages("/home/douglas/dev/tcc/data/images/solid-nodules-with-attributes/");
+		ssnwa_images.downloadImages(imagesPath + "solid-nodules-with-attributes/");
 		
 		System.out.println("FINISHED");
 	}
