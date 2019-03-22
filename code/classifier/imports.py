f671c1f161072fd6692196d968d25e1461927dc8
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-03-21 23:24:33 -0300

    Import list updated

--- code/classifier/imports.py
+++ code/classifier/imports.py
@@ -1,12 +1,25 @@
+import math
+import itertools
+import re
 import os
 import time
 import gc
+import shutil
+import imageio
+
 import numpy as np
+import tensorflow as tf
+
+from scipy import misc
+from scipy.ndimage import rotate
+
 from hyperopt import Trials, STATUS_OK, tpe
 from hyperas import optim
 from hyperas.distributions import choice, uniform
+
 from sklearn.metrics import confusion_matrix, roc_curve, auc
-import tensorflow as tf
+from sklearn.model_selection import KFold
+
 from keras import backend as K
 from keras import optimizers
 from keras.layers import Conv3D, MaxPool3D, Flatten, Dense, Dropout, Input
@@ -15,8 +28,10 @@ from keras.models import Model
 from keras.callbacks import EarlyStopping
 from keras.utils.vis_utils import plot_model, model_to_dot
 from keras.backend.tensorflow_backend import set_session
-import keras_metrics as km
 from keras.utils import to_categorical
+import keras_metrics as km
+
 from IPython.display import SVG
+from tqdm import tqdm
 
 from import_images import get_folds
