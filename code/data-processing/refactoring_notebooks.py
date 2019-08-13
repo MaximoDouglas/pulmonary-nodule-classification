d0b29a7b5f65af97392be5b03a99f116fb827b63
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-08-13 07:24:53 -0300

    imagens importantes

--- code/data-processing/refactoring_notebooks.py
+++ code/data-processing/refactoring_notebooks.py
@@ -4,16 +4,32 @@ import os
 import matplotlib.pyplot as plt
 from scipy import stats
 from collections import Counter
+from sklearn.preprocessing import MinMaxScaler
 
 '''file_name  = '../../data/features/solidNodules.csv'
 
 df = pd.read_csv(file_name)
 
-malignancies = df[df.columns[-2]]
+scaler = MinMaxScaler(copy=False)
+X = scaler.fit_transform(df[df.columns[2:73]])
+X = pd.DataFrame(X)
+
+print(np.max(df[df.columns[3]]))
+print(df.columns[3])
+
+df[df.columns[3]].plot.kde()
+plt.legend(["DPVO"])
+#X[X.columns[1]].plot.kde()
+#plt.legend(["DPVN"])
+plt.ylabel("Densidade de Probabilidade")
+plt.xlabel("Entropia")
+plt.show()'''
+
+'''malignancies = df[df.columns[-2]]
 
 dist = malignancies.value_counts()
 
-print(dist)'''
+print(dist)
 
 images_dir = '../../data/images/solid-nodules-with-attributes/'
 
@@ -42,14 +58,23 @@ for malignancy in sub:
 df_ben = pd.DataFrame(summary['benigno'])
 df_mal = pd.DataFrame(summary['maligno'])
 
-'''fig, ax = plt.subplots()
+fig, ax = plt.subplots()
 ax.boxplot([summary['maligno'], summary['benigno']])
 ax.set_xticklabels(['Malignos', 'Benignos'])
 ax.yaxis.set_ticks(np.arange(0, 50, 2))
-plt.show()'''
+plt.show()
 
 print("Df Ben min/max: " + str(df_ben[0].min()) + ' / ' + str(df_ben[0].max()))
 print("Df Mal min/max: " + str(df_mal[0].min()) + ' / ' + str(df_mal[0].max()))
 
 print("Df Ben: " + str(df_ben[0].median()) + ' (+/- ' + str(df_ben[0].std()) + ')')
-print("Df Mal: " + str(df_mal[0].median()) + ' (+/- ' + str(df_mal[0].std()) + ')')
+print("Df Mal: " + str(df_mal[0].median()) + ' (+/- ' + str(df_mal[0].std()) + ')')'''
+
+c_expon_gen     = stats.expon(scale=100, loc=0)
+gamma_expon_gen = stats.expon(scale=0.1, loc=0)
+
+x = np.linspace(0, 500, 500)
+
+plt.plot(x, c_expon_gen.pdf(x), 'b-')
+plt.title('Gamma space Distribution')
+plt.show()
