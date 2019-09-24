286b7726c8f61f6fd5012a3208ac401098afd4f2
MaximoDouglas <douglas.h.maximo@gmail.com>
2019-09-23 22:43:05 -0300

    master Commit the last changes to the master
    - Add codes of experimentations
    - Delete most of the image files
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- code/data-processing/refactoring_notebooks.py
+++ code/data-processing/refactoring_notebooks.py
@@ -4,26 +4,29 @@ import os
 import matplotlib.pyplot as plt
 from scipy import stats
 from collections import Counter
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-'''file_name  = '../../data/features/solidNodules.csv'
+file_name  = '../../data/features/solidNodules.csv'
 
 df = pd.read_csv(file_name)
+print(df.columns[3])
 
-scaler = MinMaxScaler(copy=False)
+'''print("max before: " + str(np.max(df[df.columns[3]])))
+df[df.columns[3]].plot.kde()
+plt.legend(["DPVO"])'''
+
+#scaler = MinMaxScaler(copy=False)
+scaler = StandardScaler(copy=True)
 X = scaler.fit_transform(df[df.columns[2:73]])
 X = pd.DataFrame(X)
 
-print(np.max(df[df.columns[3]]))
-print(df.columns[3])
+print("max after: " + str(np.max(X[X.columns[0]])))
+X[X.columns[0]].plot.kde()
+plt.legend(["DPVN"])
 
-df[df.columns[3]].plot.kde()
-plt.legend(["DPVO"])
-#X[X.columns[1]].plot.kde()
-#plt.legend(["DPVN"])
 plt.ylabel("Densidade de Probabilidade")
 plt.xlabel("Entropia")
-plt.show()'''
+plt.show()
 
 '''malignancies = df[df.columns[-2]]
 
@@ -74,9 +77,28 @@ print("Df Mal: " + str(df_mal[0].median()) + ' (+/- ' + str(df_mal[0].std()) + '
 gamma_expon_gen = stats.expon(scale=0.1, loc=0)
 '''
 
-gen = stats.norm(scale=0.05, loc=0.4)
+'''gen = stats.norm(scale=0.05, loc=0.4)
 x = np.linspace(0, 0.5, 100)
 
 plt.plot(x, gen.pdf(x), 'b-')
 plt.title('Dropout space Distribution')
-plt.show()
+plt.show()'''
+
+'''indx = [2, 3, 4, 6, 9, 11, 12, 18, 19, 22, 26, 28, 29, 
+      30, 31, 32, 33, 35, 38, 40, 41, 44, 46, 47, 48, 
+      50, 53, 54, 56, 57, 59, 60, 62, 63, 65, 67, 71, 72]
+
+for ind in indx:
+  print(df.columns[ind], end=", ")
+
+print(len(indx))'''
+
+'''\item ATI: energia, entropia, kurtosis, intensidade média, intensidade mínima, raiz quadrada média, skewness
+\item AF: desproporção esférica, esfericidade, relação superfícia volume 
+\item AT a 0º: entropia, homogeneidade, correlação, matiz, proeminência, variância, momento da diferenca inverso 
+\item AT a 135º: entropia, correlação, proeminência, variância 
+\item AT a 45º: entropia, homogeneidade, correlação, matiz, variância 
+\item AT a 90º: entropia, contraste, correlação, matiz, variância, momento da diferenca inverso
+\item ANB: soma dos valores, soma dos quadrados, média aritmética, variância da população, medida de skewness, segundo momento central
+
+'''
